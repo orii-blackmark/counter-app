90572d7376f1d85b5edd9b8798326cb35b67ad30
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-08 23:58:34 +0300

    Rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3", "tag4"],
   };
   styles = {
     fontSize: 20,
@@ -18,6 +19,11 @@ class Counter extends Component {
         <button style={{ fontSize: 20 }} className="btn btn-secondary btn-sm">
           Increment
         </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
